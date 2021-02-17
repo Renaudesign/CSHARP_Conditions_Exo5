ea5f2b46db93c7e15cc01ebd9417988a0f80e8da
renaudesign <renaud.sourmail@gmail.com>
2021-02-17 14:19:29 +0100

    exo5 terminé

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CSHARP_Conditions_Exo5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bonjour Utilisateur. Saisir le nom d'un mois. ");
+            string mois = Console.ReadLine().ToLowerInvariant();
+
+            switch (mois)
+            {
+                case "decembre":
+                case "janvier":
+                case "fevrier":
+                    Console.WriteLine("L'HIVER VIENT");
+                    break;
+                case "mars":
+                case "avril":
+                case "mai":
+                    Console.WriteLine("Le printemps arrive");
+                    break;
+                case "juin":
+                case "juillet":
+                case "aout":
+                    Console.WriteLine("Janis, range ton spliff et vient chanter Summertime");
+                    break;
+
+                default:
+                    Console.Write("Echec système : mauvais identifiant ou mot de passe");
+                    break;
+            }
+
+        }
+    }
+}
